c59c4754ec4bfa511bdf9135d21e82a4ecd6d1a3
emrergin <emrergin2757@yahoo.com>
2021-12-24 14:32:52 +0300

    the game is playable now, not winnable, though.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tiktakto
-Tic-Tac-Toe for The Odin Project
+Tic-Tac-Toe for [The Odin Project](https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/tic-tac-toe)
+
+This is mainly an exercise for **Factory Functions** and the **Module Pattern**.
+
+I will also try to implement an AI, my first ever.
---
+++ script.js
@@ -0,0 +1,74 @@
+const grid = document.querySelector('#container');
+
+const playerFactory = (isim, sembol,soyut) => {
+    return { isim, sembol ,soyut}
+};
+
+const Player1 = playerFactory(`1`, `X`,1);
+const Player2 = playerFactory(`2`, `O`,-1);
+const PlayerAI = playerFactory(`AI`, `O`,-1);
+
+
+const tahtamiz = (function tahtaOlustur() {
+    let currentPlayer = Player1;
+    let Tahta = [[, ,], [, ,], [, ,]];
+    let TahtaSoyut = [[0,0,0], [0,0,0], [0,0,0]];
+    const boyut = 3;
+    // tahtaTemizle();
+    for (let i = 0; i < boyut; i++) {
+        let hucreSatiri = document.createElement('div');
+        hucreSatiri.classList.add('hucreSatiri');
+        hucreSatiri.classList.add('unselectable');
+        hucreSatiri.setAttribute(`draggable`, "false");
+        for (let j = 0; j < boyut; j++) {
+            let hucre = document.createElement('div');
+            hucre.classList.add('hucre');
+            hucre.setAttribute(`data-satir`, i);
+            hucre.setAttribute(`data-sutun`, j);
+            hucre.classList.add('unselectable');
+            hucre.setAttribute(`draggable`, "false");
+            hucre.style.cssText = `width: ${100 / boyut}%`;
+            hucreSatiri.appendChild(hucre);
+        }
+        grid.appendChild(hucreSatiri);
+    }
+    const hucreler = document.querySelectorAll('.hucre');
+    hucreler.forEach((hucreYeri) => {
+        hucreYeri.addEventListener('click', sekilkoy);
+        hucreYeri.addEventListener("contextmenu", (e) => { e.preventDefault(); return false; });
+    });
+    tahtayiYenile();
+
+    function sekilkoy(e) {
+        Tahta[e.target.dataset.satir][e.target.dataset.sutun] = currentPlayer.sembol;
+        TahtaSoyut[e.target.dataset.satir][e.target.dataset.sutun] = currentPlayer.soyut;
+        tahtayiYenile();
+        zaferKontrolu();
+        if (currentPlayer === Player1) {
+            currentPlayer = Player2;
+        }
+        else {
+            currentPlayer = Player1;
+        }
+    }
+
+    function tahtayiYenile() {
+        hucreler.forEach((hucreYeri) => {
+            hucreYeri.textContent = Tahta[hucreYeri.dataset.satir][hucreYeri.dataset.sutun];
+        });
+    }
+
+    function zaferKontrolu() {
+        // for (let i = 0; i < boyut; i++) {
+        //     if (TahtaSoyut)
+        // }
+        // for (let i = 0; i < boyut; i++) {
+
+        // }
+    }
+    // return {hucreler};
+})();
+
+
+
+// tahtaOlustur();
