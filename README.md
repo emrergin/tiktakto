0376703656d6a536c02b4024230528a04d063713
emrergin <emrergin2757@yahoo.com>
2021-12-28 22:03:37 +0300

    fully working AI

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ This is mainly an exercise for **Factory Functions** and the **Module Pattern**.
 
 I also implemented my first AI, ever.
 
-However, the solution I personally found was not using any implementation of [minimax algorithm](https://en.wikipedia.org/wiki/Minimax), meaning the AI will not be able to see *k* steps ahead, and could only do, local calculations to use some heuristic. While it was not bad, it was not perfect, hence I tried to understand the minimax algorithm. During my research, I discovered the great website, [Geeks for Geeks](https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/) that includes an almost fully working code, albeit with some mistakes. This did not feel like reinventing the wheel, more of a learning opportunity.
+However, the solution I personally found was not using any implementation of [minimax algorithm](https://en.wikipedia.org/wiki/Minimax), meaning the AI will not be able to see *k* steps ahead, and could only do, local calculations to use some heuristic. While it was not bad, it was not perfect, hence I tried to understand the minimax algorithm. During my research, I discovered the great website, [Geeks for Geeks](https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/) that includes a fully working code, albeit with some not so best practices. This did not feel like reinventing the wheel, more of a learning opportunity.
 
 ### What I learned:
 - Heuristics is not enough even for solution of such a simple game.
 - Javascript can not natively copy multidimensional arrays. Thanks [Brian](https://www.linkedin.com/in/brianbucklew)!
 - For a recursive function, using literal values like strings as parameters is a much better solution.
+- The number zero is a falsy expression as much as undefined is, so for functions possibly returning zeros, just getting a return does not ensure its truth value.
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let TahtaSoyut = [[0,0,0], [0,0,0], [0,0,0]];
-
 const tahtamiz = (function anaModul() {
     const grid = document.querySelector('#container');
 
@@ -29,7 +27,7 @@ const tahtamiz = (function anaModul() {
     }
     let currentPlayer = Player1;
     let Tahta = [[,,], [,,], [,,]];
-    
+    let TahtaSoyut = [[0,0,0], [0,0,0], [0,0,0]];
     let cepheler = [[[0,0],[0,1],[0,2]],[[1,0],[1,1],[1,2]],[[2,0],[2,1],[2,2]],[[0,0],[1,0],[2,0]],[[0,1],[1,1],[2,1]],[[0,2],[1,2],[2,2]],[[0,0],[1,1],[2,2]],[[2,0],[1,1],[0,2]]];
     const boyut = 3;
 
@@ -114,92 +112,98 @@ const tahtamiz = (function anaModul() {
             }
         }
     }
-
-    // function getMin(a) {
-    //     return Math.min(...a.map(e => Array.isArray(e) ? getMin(e) : e));
-    // }
     
     // YAPAY ZEKA BURADA GIZLI====================================
-    // function eniyiHamleyiYap(tahta){
-    //     let eniyiHamle=[,];
-    //     let enIyiDeger=1000;
-    //     for (let i = 0; i < boyut; i++) {            
-    //         for (let j = 0; j < boyut; j++) {
-    //             if (tahta[i][j]===0){
-    //                 let deneTahta = [];
-    //                 for (let x = 0; x < tahta.length; x++){
-    //                     deneTahta[x] = tahta[x].slice();
-    //                 }
-    //                 deneTahta[i][j]=-1;
-    //                 if (DegerHesaplama(deneTahta,-1,1)<enIyiDeger)
-    //                 {
-    //                     enIyiDeger=DegerHesaplama(deneTahta,-1,1);
-    //                     // console.log(DegerHesaplama(deneTahta,-1,1));
-    //                     eniyiHamle=[i,j];
-    //                 }
-    //                 deneTahta[i][j]=0;
-    //             }
-    //         }
-    //     }
-    //     return eniyiHamle;
-    // }
-
-    // function hamleDegerleriBul(tahta){
-    //     let hamleDegerleri=[[1000,1000,1000],[1000,1000,1000],[1000,1000,1000]];
-
-    //     for (let i = 0; i < boyut; i++) {            
-    //         for (let j = 0; j < boyut; j++) {
-    //             if (tahta[i][j]===0){
-    //                 tahta[i][j]=-1;
-    //                 hamleDegerleri[i][j]=DegerHesaplama(tahta,-1,1);
-    //                 console.log(tahta);
-    //                 tahta[i][j]=0;
-    //             }
-    //         }
-    //     }
-    //     return hamleDegerleri;
-    // }
-
-    // function durumDegeri(sonTahta){
-    //     for (let i = 0; i < 8; i++) {
-    //         if(sonTahta[cepheler[i][0][0]][cepheler[i][0][1]]+sonTahta[cepheler[i][1][0]][cepheler[i][1][1]]+
-    //             sonTahta[cepheler[i][2][0]][cepheler[i][2][1]]===3)
-    //             {return 100;}
-    //         else if (sonTahta[cepheler[i][0][0]][cepheler[i][0][1]]+sonTahta[cepheler[i][1][0]][cepheler[i][1][1]]+
-    //             sonTahta[cepheler[i][2][0]][cepheler[i][2][1]]===-3)
-    //             {return -100;}                
-    //     }
-    //     if (!((sonTahta[0].some(item => item === 0)||sonTahta[1].some(item => item === 0)||sonTahta[2].some(item => item === 0)))){
-    //         return 0;
-    //     }
-    // }
-
-
-    // function DegerHesaplama(mevcutTahta,kisi,num){
-    //     let enOptDeger=-kisi*1000;
-
-    //     if(durumDegeri(mevcutTahta)){
-    //         return durumDegeri(mevcutTahta)-num*kisi;
-    //     }
-    //     else {
-    //         for (let i = 0; i < boyut; ++i){
-    //             for (let j = 0; j < boyut; ++j){
-    //                 if (mevcutTahta[i][j]===0){
-    //                     let yedekTahta = [];
-    //                     for (let x = 0; x < mevcutTahta.length; x++){
-    //                         yedekTahta[x] = mevcutTahta[x].slice();
-    //                     }
-    //                     yedekTahta[i][j]=kisi;
-    //                     kisi===1? enOptDeger=Math.max(enOptDeger,DegerHesaplama(yedekTahta,-kisi,num+1)) : enOptDeger=Math.min(enOptDeger,DegerHesaplama(yedekTahta,-kisi,num+1));
-    //                     yedekTahta[i][j]=0;                    
-    //                 }    
-    //             }               
-    //         }
-    //     }
-    //     return enOptDeger;
-    // }       
-
-
+    function eniyiHamleyiYapStr(tahtaStr){
+        let eniyiHamle=-1;
+        let enIyiDeger=1000;
+        for (let i = 0; i < 9; i++){           
+            if (tahtaStr.charAt(i)===`-`){
+                tahtaStr=tahtaStr.slice(0,i)+`O`+tahtaStr.slice(i+1);
+                if (DegerHesaplamaStr(tahtaStr,1,1)<enIyiDeger){
+                    enIyiDeger=DegerHesaplamaStr(tahtaStr,1,1);
+                    eniyiHamle=i;
+                }
+                tahtaStr=tahtaStr.slice(0,i)+`-`+tahtaStr.slice(i+1);                
+            }                  
+        }
+        return [Math.floor(eniyiHamle/3),eniyiHamle%3];
+    }
+    
+    function HucreDegerlesene(tahtaStr){
+        let HucreDegerleri=[[,,],[,,],[,,]];
+        for (let i = 0; i < 9; i++){           
+            if (tahtaStr.charAt(i)===`-`){
+                tahtaStr=tahtaStr.slice(0,i)+`O`+tahtaStr.slice(i+1);
+                HucreDegerleri[Math.floor(i/3)][i%3]=DegerHesaplamaStr(tahtaStr,-1,1);
+                tahtaStr=tahtaStr.slice(0,i)+`-`+tahtaStr.slice(i+1);                
+            }                  
+        }
+        return HucreDegerleri;
+    }
+    
+    function DegerHesaplamaStr(mevcutTahta,kisi,num){
+        let enOptDeger=-kisi*1000;
+    
+        if(typeof durumDegeriStr(mevcutTahta)==="number"){
+            return durumDegeriStr(mevcutTahta)-kisi*10*num;
+        }
+        else {
+            if (kisi===1){
+                for (let i = 0; i < 9; i++){           
+                    if (mevcutTahta.charAt(i)===`-`){
+                        mevcutTahta=mevcutTahta.slice(0,i)+`X`+mevcutTahta.slice(i+1);
+                        // console.log(mevcutTahta);
+                        enOptDeger=Math.max(enOptDeger,DegerHesaplamaStr(mevcutTahta,-kisi,num+1));
+                        mevcutTahta=mevcutTahta.slice(0,i)+`-`+mevcutTahta.slice(i+1);                
+                    }                  
+                }
+            }
+            if (kisi===-1){
+                for (let i = 0; i < 9; i++){           
+                    if (mevcutTahta.charAt(i)===`-`){
+                        mevcutTahta=mevcutTahta.slice(0,i)+`O`+mevcutTahta.slice(i+1);
+                        enOptDeger=Math.min(enOptDeger,DegerHesaplamaStr(mevcutTahta,-kisi,num+1));
+                        mevcutTahta=mevcutTahta.slice(0,i)+`-`+mevcutTahta.slice(i+1);                
+                    }                  
+                }
+            }
+        
+            return enOptDeger-kisi*10*num;
+        }
+    }    
+    
+    let cephelerStr=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+    function durumDegeriStr(strTahta){    
+        for (let i = 0; i < 8; i++) {
+            if(strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][1])&& strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][2]) &&  strTahta.charAt(cephelerStr[i][0])===`X`){
+                return 100;
+            }                
+            else if(strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][1])&& strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][2]) &&  strTahta.charAt(cephelerStr[i][0])===`O`){
+                return -100;
+            }                
+        }
+        if (strTahta.indexOf(`-`)===-1){
+            return 0;
+        }
+    }
+    function makeaString(stringOlmayan){
+        let sonuc=``;
+        for (let i = 0; i < 3; ++i){
+            for (let j = 0; j < 3; ++j){
+                if (stringOlmayan[i][j]===0){
+                    sonuc+=`-`;
+                }
+                if (stringOlmayan[i][j]===1){
+                    sonuc+=`X`;
+                }
+                if (stringOlmayan[i][j]===-1){
+                    sonuc+=`O`;
+                }
+            }
+        }
+        return sonuc;
+    }
     
     function SiradakiHamle(){
         if (!((TahtaSoyut[0].some(item => item === 0)||TahtaSoyut[1].some(item => item === 0)||TahtaSoyut[2].some(item => item === 0)))&&OyunModu===1){
@@ -208,9 +212,7 @@ const tahtamiz = (function anaModul() {
         }
             
         if (currentPlayer===PlayerAI && OyunModu===1){
-            // let oyunyeri=eniyiHamleyiYap(TahtaSoyut);
             let oyunyeri=eniyiHamleyiYapStr(makeaString(TahtaSoyut));
-            console.log(oyunyeri);
             Tahta[oyunyeri[0]][oyunyeri[1]] = PlayerAI.sembol;
             TahtaSoyut[oyunyeri[0]][oyunyeri[1]] = PlayerAI.soyut;
             tahtayiYenile();
@@ -232,101 +234,5 @@ const tahtamiz = (function anaModul() {
         yenidenBaslatDegistir();
     }
 
-    return {tahtaTemizle,TahtaSoyut,Tahta};
+    return {tahtaTemizle};
 })();
-
-function eniyiHamleyiYapStr(tahtaStr){
-    let eniyiHamle=-1;
-    let enIyiDeger=1000;
-    for (let i = 0; i < 9; i++){           
-        if (tahtaStr.charAt(i)===`-`){
-            tahtaStr=tahtaStr.slice(0,i)+`O`+tahtaStr.slice(i+1);
-            if (DegerHesaplamaStr(tahtaStr,-1,1)<enIyiDeger){
-                enIyiDeger=DegerHesaplamaStr(tahtaStr,-1,1);
-                eniyiHamle=i;
-            }
-            tahtaStr=tahtaStr.slice(0,i)+`-`+tahtaStr.slice(i+1);                
-        }                  
-    }
-    console.log(eniyiHamle);
-    return [Math.floor(eniyiHamle/3),eniyiHamle%3];
-}
-
-function HucreDegerlesene(tahtaStr){
-    let HucreDegerleri=[[,,],[,,],[,,]];
-    for (let i = 0; i < 9; i++){           
-        if (tahtaStr.charAt(i)===`-`){
-            tahtaStr=tahtaStr.slice(0,i)+`O`+tahtaStr.slice(i+1);
-            HucreDegerleri[Math.floor(i/3)][i%3]=DegerHesaplamaStr(tahtaStr,-1,1);
-            tahtaStr=tahtaStr.slice(0,i)+`-`+tahtaStr.slice(i+1);                
-        }                  
-    }
-    // console.log(eniyiHamle);
-    return HucreDegerleri;
-}
-
-function DegerHesaplamaStr(mevcutTahta,kisi,num){
-    // console.log(`Derinlik: `+num);
-    let enOptDeger=-kisi*1000;
-
-    if(durumDegeriStr(mevcutTahta)){
-        // console.log(`Deger: `+durumDegeriStr(mevcutTahta)-kisi*num);
-        return durumDegeriStr(mevcutTahta)-kisi*10*num;
-    }
-    else {
-        if (kisi===1){
-            for (let i = 0; i < 9; i++){           
-                if (mevcutTahta.charAt(i)===`-`){
-                    mevcutTahta=mevcutTahta.slice(0,i)+`X`+mevcutTahta.slice(i+1);
-                    // console.log(mevcutTahta);
-                    enOptDeger=Math.max(enOptDeger,DegerHesaplamaStr(mevcutTahta,-kisi,num+1));
-                    mevcutTahta=mevcutTahta.slice(0,i)+`-`+mevcutTahta.slice(i+1);                
-                }                  
-            }
-        }
-        if (kisi===-1){
-            for (let i = 0; i < 9; i++){           
-                if (mevcutTahta.charAt(i)===`-`){
-                    mevcutTahta=mevcutTahta.slice(0,i)+`O`+mevcutTahta.slice(i+1);
-                    // console.log(mevcutTahta);
-                    enOptDeger=Math.min(enOptDeger,DegerHesaplamaStr(mevcutTahta,-kisi,num+1));
-                    mevcutTahta=mevcutTahta.slice(0,i)+`-`+mevcutTahta.slice(i+1);                
-                }                  
-            }
-        }
-    
-        return enOptDeger-kisi*10*num;
-    }
-}    
-
-cephelerStr=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-function durumDegeriStr(strTahta){
-    for (let i = 0; i < 8; i++) {
-        if(strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][1])&& strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][2]) &&  strTahta.charAt(cephelerStr[i][0])===`X`){
-            return 100;
-        }                
-        else if(strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][1])&& strTahta.charAt(cephelerStr[i][0])===strTahta.charAt(cephelerStr[i][2]) &&  strTahta.charAt(cephelerStr[i][0])===`O`){
-            return -100;
-        }                
-    }
-    if (strTahta.indexOf(`-`)===-1){
-        return 0;
-    }
-}
-function makeaString(stringOlmayan){
-    let sonuc=``;
-    for (let i = 0; i < 3; ++i){
-        for (let j = 0; j < 3; ++j){
-            if (stringOlmayan[i][j]===0){
-                sonuc+=`-`;
-            }
-            if (stringOlmayan[i][j]===1){
-                sonuc+=`X`;
-            }
-            if (stringOlmayan[i][j]===-1){
-                sonuc+=`O`;
-            }
-        }
-    }
-    return sonuc;
-}
